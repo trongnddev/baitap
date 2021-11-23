f677471c39bef93f6371902b84e1971bfaa95b22
trongnd <nguyendotrong.58@gmail.com>
2021-11-23 22:30:56 +0700

    [baitap6] cap nhat document cho phuong thuc

--- baitap6/baitap6.rb
+++ baitap6/baitap6.rb
@@ -1,19 +1,26 @@
-numbers = [3, 5, -4, 8, 11, -2, 6]
-target_sum = 10
-hash_temp = Hash.new
-result = []
-
-#Sau moi lan lap kiem tra trong hash co key nao bang val khong
-#neu co thi thi do la cap key value can tim
-#neu khong thi them phan tu co key la val con value la target_num -val
-numbers.each do |val|
+# @param [integer] target_sum
+# @param [array] numbers has integer numbers
+# @return [array]  has two integer
+def find_two_numbers(target_sum, numbers)
     
-    if hash_temp.key?(val)
-        result.push(target_sum - val, val)
-        break
-    else
-        hash_temp[target_sum - val] = val
+    result = []
+    hash_temp = Hash.new
+
+    #Sau moi lan lap kiem tra trong hash co key nao bang val khong,
+    #neu co thi thi do la cap key value can tim,
+    #neu khong thi them phan tu co key la target_num - val, value la val.
+    numbers.each do |val|
+        if hash_temp.key?(val)
+            result.push(target_sum - val, val)
+            break
+        else
+            hash_temp[target_sum - val] = val
+        end
     end
+    return result
 end
 
-puts "mang chua hai so co tong bang #{target_sum}: #{result}"
+numbers = [3, 5, -4, 8, 11, -2, 6]
+target_sum = 10
+find_two_numbers(target_sum,numbers)
+puts "mang chua hai so co tong bang #{target_sum}: #{find_two_numbers(target_sum,numbers)}"
