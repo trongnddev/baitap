8ab720858f5aaa9dfb760151dc960b34ceef162f
VuVT <truongvuuit@gmail.com>
2021-12-04 10:05:56 +0700

    Review ex8

--- baitap8/baitap8.rb
+++ baitap8/baitap8.rb
@@ -19,3 +19,10 @@ end
 a1 = [5, 1, 22, 26, 6, -1, 8, 10]
 b1 = [1, 6, -1, 10]
 puts is_sub_array?(a1, b1)
+
+# Code e viết ngắn gọn, trình bày rõ ràng, có comment dễ đọc, có early return. Comment khi e vào dự án thực tế thì nên viết bằng tiếng anh nhé e.
+# Line 10, với những đk if phức tạp, e có thể xem xét để tách hàm và tên hàm mang ý nghĩa của nội dụng comment ở trên, lúc này e kg cần
+# comment line 9 cũng được ah, sau này khi nhìn vào tên hàm e sẽ biết được hàm để l
+# Line 15 e viết chữ lengt sai nhé
+# array và check_array đều là mảng chứ nhiều phần tử -> nên là số nhiều nhé e
+# Format commit git của e a thấy chưa đúng lắm
