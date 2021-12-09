2c5558a9185d909c506a749086333e30ac988861
trongnd <nguyendotrong.58@gmail.com>
2021-12-09 22:17:22 +0700

    [baitap10] had find arrays three numbers

--- baitap10/baitap10.rb
+++ baitap10/baitap10.rb
@@ -16,14 +16,14 @@ def find_arrays_three_numbers(target_sum, numbers)
    
     target_sum_arrays = get_subtract(numbers,target_sum)
 
-    target_sum_arrays.each_with_index do |val, index|
+    target_sum_arrays[0..-3].each_with_index do |val, index|
         hash_temp = {}
-        for i in (index+1)..(numbers.length-1) do
+        numbers[index+1..-1].each_with_index do |valI, indexi|
 
-            if hash_temp.key?(val - numbers[i])
-                result.push([target_sum-val, val - numbers[i], numbers[i]])
+            if hash_temp.key?(val - valI)
+                result.push([target_sum-val, val - valI, valI])
             else
-                hash_temp[numbers[i]] = true 
+                hash_temp[valI] = true 
             end
         end       
     end
@@ -31,8 +31,7 @@ def find_arrays_three_numbers(target_sum, numbers)
 end
 
 
-numbers = [12, 3, 1, 2, -6, 5, -8, 6]
+numbers = [12, 3, 1, 2, -6, 5, -8, 6, 0, -1]
 target_sum = 0
 
 puts "nhung bo 3 so co tong bang #{target_sum}: #{find_arrays_three_numbers(target_sum,numbers)}"
-
